4882a5501d2bb37f2f3b02edf224b10c40e3d142
JauniKapauni <riches-goods.54@icloud.com>
2026-05-15 12:06:30 +0200

    feat: add first_join stat

--- src/main/java/de/jaunikapauni/axstats/manager/DatabaseManager.java
+++ src/main/java/de/jaunikapauni/axstats/manager/DatabaseManager.java
@@ -37,7 +37,7 @@ public class DatabaseManager {
 
     public boolean initDatabaseTable1(){
         try(Connection conn = getConnection()){
-            try(PreparedStatement ps = conn.prepareStatement("CREATE TABLE IF NOT EXISTS players (uuid VARCHAR(255) NOT NULL PRIMARY KEY, deaths INT, sessions INT, isOnline BOOLEAN)")){
+            try(PreparedStatement ps = conn.prepareStatement("CREATE TABLE IF NOT EXISTS players (uuid VARCHAR(255) NOT NULL PRIMARY KEY, deaths INT, sessions INT, isOnline BOOLEAN, first_join DATETIME)")){
                 ps.executeUpdate();
                 return true;
             }
