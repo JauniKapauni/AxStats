2dc7feb4a8fed709595dfd3475b43d7c2a8ca8fa
JauniKapauni <riches-goods.54@icloud.com>
2026-05-15 11:49:05 +0200

    init: initialize project

---
+++ src/main/java/de/jaunikapauni/axstats/AxStats.java
@@ -0,0 +1,16 @@
+package de.jaunikapauni.axstats;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AxStats extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
